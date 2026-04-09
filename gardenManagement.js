1164e982ea96e7a19b84b2c91848f2c3ba405990
lysa omondi <lisabethkidangasi@gmail.com>
2026-04-09 03:48:33 +0300

    statements

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,20 @@
 const temperature = 90;
-const timeOfDay = "morning";
-let soilMoisture = 30;
+if(temperature > 80){
+    console.log("watering on");
+}
+else{
+    console.log("watering off");
+}
+const timeDay = "morning";
+if(timeDay === "evening"){
+    console.log("Night", "Lights on");
+}
+else{
+    console.log("Lights off");
+}
+let soilMoisture = 0;
+
+while (soilMoisture <= 40) {
+    console.log(soilMoisture);
+    soilMoisture += 5;
+}
